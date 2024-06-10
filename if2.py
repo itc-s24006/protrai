c6be9bb512cdf637e726d409ee557939b54d2368
itc-s24006 <s24006@std.it-college.ac.jp>
2024-06-10 15:32:00 +0900

    add if_kadai.py

---
+++ if2.py
@@ -0,0 +1,10 @@
+#s24006
+#条件式プログラム
+
+score = 60
+if score >= 80:
+    print("やるやん")
+    print("その調子")
+else:
+    print("どんまい")
+    
---
+++ if_kadai.py
@@ -0,0 +1,14 @@
+#s24006
+#score判定プログラム
+
+
+score = 85
+if score >= 60:
+    print("可")
+elif score >= 70:
+    print("良")
+elif score >= 80:
+    print("優")
+else:
+    print("不可")
+
