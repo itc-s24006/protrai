d9db68afae67d56f6399c88fa99ab63c9324e097
itc-s24006 <s24006@std.it-college.ac.jp>
2024-07-09 09:18:22 +0900

    add digit file

---
+++ digitsImage50.py
@@ -0,0 +1,14 @@
+#s24006
+#
+
+import sklearn.datasets
+import matplotlib.pyplot as plt
+
+digits = sklearn.datasets.load_digits()
+
+for i in range(50):
+    plt.subplot(5,10, i + 1) #5x10に順番に表示する
+    plt.axis("off") #枠線を非表示
+    plt.title(digits.target[i]) #この数字は何か
+    plt.imshow(digits.images[i], cmap="Greys")
+plt.show()
