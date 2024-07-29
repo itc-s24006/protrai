4e98f6202bd7a25536a917fd440a00780ea17365
itc-s24006 <s24006@std.it-college.ac.jp>
2024-07-29 16:52:03 +0900

    flask_msg_rensyu.py 変更

--- flask_msg_rensyu.py
+++ flask_msg_rensyu.py
@@ -10,23 +10,31 @@ import datetime
 
 app = Flask(__name__)
 
+#ルート/にアクセスすると、index.htmlテンプレートがレンダリングされる
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#ルート/himitsuにアクセスすると、「秘密のページです」というテキストが表示される
 @app.route('/himitsu')
 def himitsu():
     return "<small>秘密のページです</small>"
 
+#ルート/msgにアクセスすると、msg.htmlテンプレートがレンダリングされる
 @app.route('/msg')
 def msg():
     return render_template('msg.html')
 
+"""
+ルート/submitはPOSTリクエストを受け付ける。
+通常、HTMLフォームの送信先として使用される
+書き込みが完了したら「書き込みましたよ」というメッセージを返す
+"""
 @app.route('/submit', methods=['POST'])
 def submit():
-    #フォームから入力された内容を取得
+    #フォームから送信されたデータ(content)を取得
     content = request.form['content']
-    #data.txtを開いてaの追記モードでデータ追記
+    #data.txtファイルを追記モードで開き、現在の日時とともにcontentの内容をファイルに書き込む
     with open('data.txt','a') as file:
         file.write(f"\n{datetime.datetime.now()}\n{content}\n")
         return """
@@ -36,6 +44,6 @@ def submit():
                 <a href="/msg"><small>もう一度送る</small></a>
                 """
 
-
+#スクリプトが直接実行された場合、Flaskアプリケーションをローカルホストのポート5000でデバッグモードで起動する
 if __name__=="__main__":
     app.run(host='0.0.0.0' ,port='5000' ,debug=True)
